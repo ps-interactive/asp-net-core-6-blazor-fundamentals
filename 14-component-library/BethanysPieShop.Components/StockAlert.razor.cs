e12e8570927f188835c426292f874ce8445adaab
Gill Cleeren <GillCleeren@users.noreply.github.com>
2023-01-06 15:22:00 +0100

    solutions init

---
+++ 14-component-library/BethanysPieShop.Components/StockAlert.razor.cs
@@ -0,0 +1,17 @@
+﻿namespace BethanysPieShop.Components
+{
+    public partial class StockAlert
+    {
+
+        public string StockMessage { get; set; }
+
+        protected override void OnInitialized()
+        {
+            int rnd = new Random().Next(10);
+            if (rnd <= 5)
+                StockMessage = "Stock is running low";
+            else
+                StockMessage = "Stock OK";
+        }
+    }
+}
---
+++ 14-component-library/BethanysPieShop/Shared/Pie.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BethanysPieShop.Shared
+{
+    public class Pie
+    {
+        public int PieId { get; set; }
+        
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(200)]
+        public string ShortDescription { get; set; } = string.Empty;
+        public string? LongDescription { get; set; }
+        public decimal Price { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? ImageThumbnailUrl { get; set; }
+        public bool IsPieOfTheWeek { get; set; }
+        public bool InStock { get; set; }
+    }
+}
