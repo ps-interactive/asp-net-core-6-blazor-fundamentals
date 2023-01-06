af83685d03a5b28b80a9cddf6cf3647c423f4465
Gill Cleeren <GillCleeren@users.noreply.github.com>
2023-01-06 16:00:30 +0100

    02

--- 02-layout-component/BethanysPieShop/Client/Pages/PieOverview.razor.cs
+++ 02-layout-component/BethanysPieShop/Client/Pages/PieOverview.razor.cs
@@ -4,12 +4,6 @@ namespace BethanysPieShop.Client.Pages
 {
     public partial class PieOverview
     {
-        public Pie RandomPie { get; set; }
-
-        protected override void OnInitialized()
-        {
-            RandomPie = new Pie { PieId = 1, Name = "Strawberry Pie", Price = 15.95M, ShortDescription = "Lorem Ipsum", LongDescription = "Icing carrot", InStock = true, IsPieOfTheWeek = false };
-            base.OnInitialized();
-        }
+        public string Message { get; set; } = "List of all pies in stock";
     }
 }
